e241324419222e78e716ddd463376081da00c520
HemantNavlani <hemant.navlani.0506@gmail.com>
2023-09-02 19:28:02 +0530

    Added Tailwind Lectures Stuff

---
+++ 22 August/tailwind/tailwind-one/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//tailwind-one % npx tailwindcss -i ./src/input.css -o ./dist/style.css --watch
+
